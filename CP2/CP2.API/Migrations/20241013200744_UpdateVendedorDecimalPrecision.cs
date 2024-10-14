a54ae7d1bf7fb55cedd20b0ab90cd770bec3b775
Patricia Naomi <patricianaomiyama@gmail.com>
2024-10-13 22:52:02 -0300

    CP2

---
+++ CP2/CP2.API/Migrations/20241013200744_UpdateVendedorDecimalPrecision.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CP2.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateVendedorDecimalPrecision : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tb_fornecedor",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Nome = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Cnpj = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Telefone = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Email = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    CriadoEm = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_fornecedor", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tb_vendedor",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Nome = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Email = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Telefone = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    DataNascimento = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false),
+                    Endereco = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    DataContratacao = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false),
+                    ComissaoPercentual = table.Column<decimal>(type: "DECIMAL(6,2)", nullable: false),
+                    MetaMensal = table.Column<decimal>(type: "DECIMAL(10,2)", nullable: false),
+                    CriadoEm = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_vendedor", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tb_fornecedor");
+
+            migrationBuilder.DropTable(
+                name: "tb_vendedor");
+        }
+    }
+}
